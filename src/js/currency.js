ba01600388c56301519b3ba67b788881cff0dbce
Donovan Weber <donovanweber03@gmail.com>
2022-07-01 17:41:23 -0400

    Add working errors for other option and entering no amount

--- src/js/currency.js
+++ src/js/currency.js
@@ -8,24 +8,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./../css/styles.css";
 import CurrencyService from "./../js/currency-service.js";
 
+function clearFields() {
+  $("#output-exchanged").empty();
+}
+function clearError() {
+  $('#output-error').empty();
+}
+
 $(document).ready(function() {
   $("#form").submit(function(event){
     event.preventDefault();
     const amount = $("#amount").val();
     const currencyToExchange = $('#currency').val();
-    console.log(amount);
     let promise = CurrencyService.getCurrency(amount, currencyToExchange);
     promise.then((response) => {
       const body = JSON.parse(response);
-      if (body !== 404) {
-        $("#output-exchange").text(`The value of ${amount} USD to ${currencyToExchange} is ${body.conversion_result}`);
+      console.log(body);
+      if (body.conversion_result > 0) {
+        $("#output-exchange").text(`The value of ${amount} USD is ${body.conversion_result} ${currencyToExchange}`);
+        clearError();
       } else {
-        $("#output-error").text(`This currency is currently unavailable: ${currencyToExchange}`);
+        $("#output-error").text(`please enter an amount to receive an exchange`);
       }
+    }, function(error) {
+      console.log(error.result);
+      if (error.result === undefined) {
+        clearFields();
+        $("#output-error").text(`This currency is unavailable: (${currencyToExchange}) please choose a different one`);
+      } else {
+        clearFields();
+        $("#output-error").text(`There was an error processing your request ${error}`);
+      }
+      return false;
     });
-  }, function(error) {
-    console.log(error);
-    $("#output-error").text(`There was an error processing your request: ${error}`);
-    return false;
   });
 });
